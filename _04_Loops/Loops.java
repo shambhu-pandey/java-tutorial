10ae0d9ec7fd1ad20cc168f91a6d45f475f2e030
java <shambhupandey1008@gmail.com>
2024-01-29 02:05:41 +0530

    for , while , do-while

---
+++ _04_Loops/Loops.java
@@ -0,0 +1,59 @@
+// FOR LOOPS 
+
+// package _03_If_else;
+
+// public class Loops {
+//   public static void main(String[] args) {
+//     for (int i = 0; i < 10; i++) {
+//       System.out.println("hello pandey ji ");
+//     }
+//   }
+// }
+
+// print the number from 1 to 10  like : 0 ,1 , ...10;
+
+// package _03_If_else;
+
+// public class Loops {
+//   public static void main(String[] args) {
+//     for (int i = 0; i <= 10; i++) {
+//       System.out.println(i);
+//     }
+//   }
+// }
+
+// WHILE LOOP 
+
+// package _03_If_else;
+
+// public class Loops {
+//   public static void main(String[] args) {
+
+//     int i = 0;
+//     while (i <= 20) {
+//       System.out.println(i);
+//       i++;
+//     }
+
+//   }
+// }
+
+// DO WHILE LOOP 
+
+package _04_Loops;
+
+public class Loops {
+  public static void main(String[] args) {
+    int i = 1;
+    do {
+      System.out.println("hello world ");
+      i++;
+    } while (i <= 10);
+
+    int j = 12;
+    do {
+      System.out.println("apple");
+      j++;
+    } while (j <= 11);
+  }
+}
