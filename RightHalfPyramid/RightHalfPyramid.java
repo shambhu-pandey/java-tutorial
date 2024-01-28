3ecfff16d11bf0ac33e22e8377ed75eb7ac3b1cd
java <shambhupandey1008@gmail.com>
2024-01-28 16:20:41 +0530

    practice question

---
+++ RightHalfPyramid/RightHalfPyramid.java
@@ -0,0 +1,11 @@
+package RightHalfPyramid;
+
+public class RightHalfPyramid {
+  public static void main(String[] a) {
+    System.out.println("* * * * *");
+    System.out.println("* * * *");
+    System.out.println("* * * ");
+    System.out.println("* * ");
+    System.out.println("* ");
+  }
+}
