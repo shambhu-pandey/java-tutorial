f896d0a2242522c480c52de1c05738008fa2f2eb
java <shambhupandey1008@gmail.com>
2024-02-04 00:13:05 +0530

    Array

---
+++ _11_Array_2d/Demo1.java
@@ -0,0 +1,83 @@
+
+//Searching for an element x in a matrix.
+
+// import java.util.Scanner;
+
+// public class Demo1 {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.println("enter row and column :");
+
+//     int row = sc.nextInt();
+//     int column = sc.nextInt();
+
+//     int number[][] = new int[row][column];
+
+//     System.out.println("enter search element :");
+
+//     int search = sc.nextInt();
+
+//     boolean found = false;
+
+//     for (int i = 0; i < row; i++) {
+//       for (int j = 0; j < column; j++) {
+//         number[i][j] = sc.nextInt();
+//       }
+//     }
+
+//     for (int i = 0; i < row; i++) {
+//       for (int j = 0; j < column; j++) {
+//         if (search == number[i][j]) {
+//           System.out.println("search element is presnt at " + (i) + ", " + (j));
+//           found = true;
+//           break;
+//         }
+
+//       }
+//       if (found) {
+//         break;
+//       }
+//     }
+//     if (!found) {
+//       System.out.println("search element is not present");
+//     }
+//   }
+
+// }
+
+import java.util.Scanner;
+
+public class Demo1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("enter row and column :");
+
+    int row = sc.nextInt();
+    int column = sc.nextInt();
+
+    int number[][] = new int[row][column];
+
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < column; j++) {
+        number[i][j] = sc.nextInt();
+      }
+    }
+
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < column; j++) {
+        System.out.print(number[i][j] + "   ");
+      }
+      System.out.println();
+    }
+    System.out.println(" ");
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < column; j++) {
+        System.out.print(number[j][i] + "   ");
+      }
+      System.out.println();
+    }
+
+  }
+}
---
+++ _11_Array_2d/theory.md
@@ -0,0 +1,7 @@
+![alt text](image.png)
+
+## Declaration of 2d array
+
+![alt text](image-1.png)
+
+![alt text](image-2.png)
